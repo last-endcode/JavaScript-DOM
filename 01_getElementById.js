98e22cd11cd67e81e2c3b790fc6558e7fdb3581e
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-25 06:37:39 +0700

    getElementById

--- 01_getElementById.js
+++ 01_getElementById.js
@@ -1,24 +1,12 @@
-// getElementById() for select only id on html
-// for giving effect too
+// getElementById for id
+// u can apply effect or whatever u want
 
-// u should use declare variable because if multiple is very efisien
+// change #container background be dark
+const background = document.getElementById('container');
+background.style.background = '#333';
+
+// change title h2 color be white, uppercase,padding
 const title = document.getElementById('title');
-title.style.color = 'blue'; //change h1 title be blue
-title.style.textTransform = 'uppercase'; //be uppercase
-
-// if not declare and use multiple method not efisien and code be long
-// document.getElementById('title').style.color = 'coral';
-// document.getElementById('title').style.textTransform = 'uppercase';
-
-// container & btn
-const container = document.getElementById('container');
-const button = document.getElementById('btn');
-
-container.style.background = '#333';
-container.style.width = '400px';
-
-button.style.textTransform = 'uppercase';
-button.style.fontSize = '1.25rem';
-button.style.background = '#ddd';
-button.style.color = '#333';
-button.style.margin = '0.75rem';
+title.style.color = '#fff';
+title.style.textTransform = 'uppercase';
+title.style.padding = '1.25rem';
