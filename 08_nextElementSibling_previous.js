45ca4417dcfe053ed3b0dc51f163156d77523be9
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-25 08:58:05 +0700

    nextElementSibling & previousElementSibling

--- 08_nextElementSibling_previous.js
+++ 08_nextElementSibling_previous.js
@@ -3,10 +3,14 @@
 // remember this element not read white space just element
 // and not same with nextSibling read white space, read comment
 
-const result = document.querySelector('#result');
-console.log(result.previousElementSibling); //hohoho
+const list_items = document.querySelector('ul');
+// previousElementSibling
+list_items.previousElementSibling.style.background = 'blue';
+// nextElementSibling ul > next or inside ul
+// output <script src=........
+const nextLi = list_items.nextElementSibling;
+console.log(nextLi);
 
-const last = document.querySelector('#last');
-const modification = last.previousElementSibling;
-modification.style.fontSize = '2rem';
-modification.style.textTransform = 'uppercase';
+const headingNext = document.querySelector('h2');
+// nextElement is ul and add background ul be red
+headingNext.nextElementSibling.style.background = 'red';
