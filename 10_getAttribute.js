8f8b343833d72f525d1346de7e901db24af763dd
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-26 13:32:37 +0700

    getAttribute() & setAttribute()

--- 10_getAttribute.js
+++ 10_getAttribute.js
@@ -1,7 +1,20 @@
 // getAttribute();
 // setAttribute();
 
-const first = document.querySelector('.first');
-// getAttribute
-const getIDs = first.getAttribute('id');
-console.log(getIDs);
+const heading = document.querySelector('.title');
+// getAttribute class and id from h2
+const classValue = heading.getAttribute('class');
+const idsValue = heading.getAttribute('id');
+console.log(classValue); // show title class
+console.log(idsValue); //show titles id
+
+// setAttribute
+// first move to p cz no have class
+const nextoP = heading.nextElementSibling.nextElementSibling;
+nextoP.setAttribute('id', 'text');
+nextoP.textContent = 'This is a text';
+
+// example again showLinks
+const links = document.querySelector('.links');
+const getLinks = links.getAttribute('href');
+console.log(getLinks);
