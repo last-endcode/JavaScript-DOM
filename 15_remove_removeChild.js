23ee27e1b046498ea2c2e75498c33693aaa92b26
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-26 16:28:16 +0700

    remove removeChild()

---
+++ 15_remove_removeChild.js
@@ -0,0 +1,9 @@
+// remove()
+// removeChild()
+
+const result = document.querySelector('.container');
+// result.remove();
+
+// here use removeChild()
+const liRHP = result.querySelector('h2');
+result.removeChild(liRHP);
