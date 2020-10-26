99ccdb1511132e83bbeb2273232f87ab9ad81161
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-26 15:59:13 +0700

    insertBefore()

--- 13_insertBefore.js
+++ 13_insertBefore.js
@@ -15,3 +15,10 @@ shp.classList.add('red');
 // insertBefore
 // rhp is element, and shp is location after rhp
 container.insertBefore(rhp, shp);
+
+// replace child
+const heading6 = document.createElement('h3');
+const textmarine = document.createTextNode('The marine ford');
+heading6.appendChild(textmarine);
+// replace child new, old
+container.replaceChild(heading6, rhp);
