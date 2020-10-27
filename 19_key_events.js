decb8a034b284c5a0991da4384e6ffcba1f3c282
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-27 15:34:10 +0700

    key events

---
+++ 19_key_events.js
@@ -0,0 +1,17 @@
+// keypress - when key is pressed
+// keydown - when key is down
+// keyup - when key is released
+
+const inputName = document.querySelector('.inputName');
+
+inputName.addEventListener('keypress', function () {
+  console.dir('u r press key');
+});
+
+// inputName.addEventListener('keydown', function () {
+//   console.log('u r press key');
+// });
+
+// inputName.addEventListener('keyup', function () {
+//   console.dir(inputName);
+// });
